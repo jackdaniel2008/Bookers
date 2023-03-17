b258e9f34646698b9d1c526bf78c569c7f6be3bf
jackdaniel2008 <126785454+jackdaniel2008@users.noreply.github.com>
2023-03-17 13:18:39 +0000

    layouts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,30 @@
 class BooksController < ApplicationController
-  def new
+  def index
+    @book = Book.new
+    @books = Book.all
   end
 
-  def index
+  def create
+    # ｢view｣ファイルへの受け渡しが必要ないためローカル変数
+    book = Book.new(book_params)
+    book.save
+    # ｢/bboks｣画面へリダイレクト
+    redirect_to "/books/#{book.id}"
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+  end
+
+  def destroy
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
